9c12c39767dcd70ac7684dde3b60d3b51151ae43
Denali Balser <denali.balser167@gmail.com>
2019-06-04 18:03:09 +0000

    first commit

---
+++ lib/compost/cli.rb
@@ -0,0 +1,8 @@
+require "compost/cli/version"
+
+module Compost
+  module Cli
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/compost/cli/version.rb
@@ -0,0 +1,5 @@
+module Compost
+  module Cli
+    VERSION = "0.1.0"
+  end
+end
